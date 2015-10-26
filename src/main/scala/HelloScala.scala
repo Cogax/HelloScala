617ce58b2204616e9a230dc8e64efced9da349ed
Andreas Gyr <andygyr@gmx.ch>
2015-10-26 21:25:31 +0100

    function type

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -4,16 +4,7 @@
 
 object HelloScala {
   def main(args: Array[String]) {
-    println(calc(3, 4, '+'));
-    println(calc(3, 4, '.'));
-  }
-
-  def calc(val1: Int, val2: Int, operator: Char): Int = {
-    return operator match {
-      case '+' => val1 + val2
-      case '-' => val1 - val2
-      case '/' => val1 / val2
-      case '*' => val1 * val2
-    }
+    val testFunc: (Int) => Int = (x) => x+1
+    println(testFunc(2));
   }
 }
